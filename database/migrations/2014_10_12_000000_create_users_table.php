b5567b9a745db5aadf14c4fc3535635f20549f6f
amirmasoud <amirmasood33@gmail.com>
2017-03-06 20:20:04 +0330

    Add php-cs file to gitignore Add Laravel Socialite Remove name from users table Add 4 roles for user in users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->enum('role', ['admin', 'author', 'publisher', 'subscriber'])->default('subscriber');
             $table->rememberToken();
             $table->timestamps();
         });
