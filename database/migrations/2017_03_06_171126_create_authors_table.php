dce02c9b3beefa8a97e11599a9abeb926b35c13c
amirmasoud <amirmasood33@gmail.com>
2017-03-06 20:57:26 +0330

    Add author tables

---
+++ database/migrations/2017_03_06_171126_create_authors_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAuthorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('authors', function (Blueprint $table) {
+            $table->increments('id');
+            $table->dateTime('born');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')
+                  ->references('id')->on('users')
+                  ->onDelete('cascade');
+            $table->integer('occupation_id')->unsigned();
+            $table->foreign('occupation_id')
+                  ->references('id')->on('occupations')
+                  ->onDelete('cascade');
+            $table->integer('movement_id')->unsigned()->nullable();
+            $table->foreign('movement_id')
+                  ->references('id')->on('movements')
+                  ->onDelete('cascade');
+            $table->text('extra')->nullable();
+            $table->boolean('approved')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('authors');
+    }
+}
