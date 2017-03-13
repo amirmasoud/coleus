cc624fc1d0d289e2b6e71d86859bb1a75702e470
amirmasoud <amirmasood33@gmail.com>
2017-03-13 12:05:49 +0330

    Add comments table

---
+++ database/migrations/2017_03_13_082824_create_comments_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('text');
+            $table->unsignedSmallInteger('rate');
+            $table->integer('book_id')->unsigned();
+            $table->foreign('book_id')
+                  ->references('id')->on('books')
+                  ->onDelete('cascade');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')
+                  ->references('id')->on('users')
+                  ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
