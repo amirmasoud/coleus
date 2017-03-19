3d2c44214461a7050d85df1c39db770d3963d092
amirmasoud <amirmasood33@gmail.com>
2017-03-19 19:23:39 +0330

    Make back to top english

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
 <div class="container marketing">
     <!-- FOOTER -->
     <footer>
-        <p class="pull-right"><a class="btn btn-link" href="#" role="button">بازگشت به بالا &uarr;</a></p>
+        <p class="pull-right"><a class="btn btn-link" href="#" role="button">Top &uarr;</a></p>
         <p class="pull-left brand-icons">
             <i class="fa fa-windows windows-color" aria-hidden="true"></i>
             <i class="fa fa-apple" aria-hidden="true"></i>
