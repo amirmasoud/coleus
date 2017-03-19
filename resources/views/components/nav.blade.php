f4ff94a5c6ad2e1983895ca93997d9ccdde8452d
amirmasoud <amirmasood33@gmail.com>
2017-03-19 19:05:41 +0330

    Change app name

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -8,7 +8,7 @@
             <span class="icon-bar"></span>
             </button>
             <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
+                {{ config('app.name', 'Negarin') }}
             </a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
