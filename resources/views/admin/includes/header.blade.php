49aec31cbcc42b03fd01becdc7db9404040639c8
amirmasoud <amirmasood33@gmail.com>
2017-03-20 20:07:05 +0330

    Refactor Admin header

--- resources/views/admin/includes/header.blade.php
+++ resources/views/admin/includes/header.blade.php
@@ -21,59 +21,58 @@
                         <!-- Notifications Menu -->
                         <li class="dropdown notifications-menu">
                             <!-- Menu toggle button -->
-                            {{-- <notification-menu user-id="{{ Auth::user()->id }}" count="{{ Auth::user()->unreadNotifications->count() }}"></notification-menu> --}}
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                <i class="fa fa-bell-o"></i>
+                                <span class="label label-warning">1</span>
+                            </a>
                             <ul class="dropdown-menu">
                                 <li>
                                     <!-- Inner Menu: contains the notifications -->
                                     <ul class="menu">
-                                        {{--                   <notification-client-count user-id="{{ Auth::user()->id }}"
-                                        count="{{ Auth::user()->countOfRegisteredClients() }}"
-                                        href="{{ route('clients.filter') }}?ids={!! Auth::user()->newClientIds() !!}&markAsRead=&#10003;">
-                                        </notification-client-count>
-                                        <notification-driver-count user-id="{{ Auth::user()->id }}"
-                                        count="{{ Auth::user()->countOfRegisteredDrivers() }}"
-                                        href="{{ route('drivers.filter') }}?ids={!! Auth::user()->newDriverIds() !!}&markAsRead=&#10003;">
-                                        </notification-driver-count> --}}
-                                    </ul>
-                                </li>
-                            </ul>
-                        </li>
-                        <!-- User Account Menu -->
-                        <li class="dropdown user user-menu">
-                            <!-- Menu Toggle Button -->
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                <!-- The user image in the navbar-->
-                                {{-- {{ HTML::image(Auth::user()->web->picture, 'User Image', ['class' => 'user-image']) }} --}}
-                                <!-- hidden-xs hides the username on small devices so only the image appears. -->
-                                <span class="hidden-xs">{{-- {{ $first_name }} --}}</span>
-                            </a>
-                            <ul class="dropdown-menu">
-                                <!-- The user image in the menu -->
-                                <li class="user-header">
-                                    {{-- {{ HTML::image(Auth::user()->web->picture, 'User Image', ['class' => 'img-circle']) }} --}}
-                                    <p>
-                                        {{--                   {{ $first_name }} {{ $last_name }}
-                                    --}}                </p>
-                                </li>
-                                <!-- Menu Footer-->
-                                <li class="user-footer">
-                                    <div class="pull-left">
-                                        <a href="{{-- {{ route('webs.edit') }} --}}" class="btn btn-default btn-flat">Profile</a>
-                                    </div>
-                                    <div class="pull-right">
-                                        <a href="{{ url('/logout') }}" class="btn btn-default btn-flat"
-                                            onclick="event.preventDefault();
-                                            document.getElementById('logout-form').submit();">
-                                            Logout
+                                        <li><!-- start notification -->
+                                        <a :href="href">
+                                            <i class="ion-android-walk text-aqua"></i> Notifications
                                         </a>
-                                        {{--                   <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form> --}}
-                                    </div>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
+                                    </li>
+                                    <!-- end notification -->
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <!-- User Account Menu -->
+                    <li class="dropdown user user-menu">
+                        <!-- Menu Toggle Button -->
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                            <!-- The user image in the navbar-->
+                            <img src="https://organicthemes.com/demo/profile/files/2012/12/profile_img.png" class="user-image" alt="User Image" width="20">
+                            <!-- hidden-xs hides the username on small devices so only the image appears. -->
+                            <span class="hidden-xs">Admin</span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <!-- The user image in the menu -->
+                            <li class="user-header">
+                                <img src="https://organicthemes.com/demo/profile/files/2012/12/profile_img.png" class="img-circle" alt="User Image">
+                                <p>Admin</p>
+                            </li>
+                            <!-- Menu Footer-->
+                            <li class="user-footer">
+                                <div class="pull-left">
+                                    <a href="#" class="btn btn-default btn-flat">Profile</a>
+                                </div>
+                                <div class="pull-right">
+                                    <a href="{{ url('/logout') }}" class="btn btn-default btn-flat"
+                                        onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">
+                                        Logout
+                                    </a>
+                                    <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                                        {{ csrf_field() }}
+                                    </form>
+                                </div>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
