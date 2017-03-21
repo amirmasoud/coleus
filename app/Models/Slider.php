df2504b0ed6c2f94f7eeb313bc08e226aaf083e8
amirmasoud <amirmasood33@gmail.com>
2017-03-21 22:52:31 +0330

    Add fillable fills and 3 helper methods

--- app/Models/Slider.php
+++ app/Models/Slider.php
@@ -3,10 +3,20 @@
 namespace App\Models;
 
 use Image;
+use Storage;
 use Illuminate\Database\Eloquent\Model;
 
 class Slider extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'sub', 'link', 'image',
+    ];
+
     /**
      * Save slider image.
      *
@@ -15,21 +25,42 @@ class Slider extends Model
      */
     public function setImageAttribute($image)
     {
-        $name = $this->attributes['image'] = basename($image->store('public/sliders/'));
-        $img = Image::make('storage/sliders/' . $name);
-        $img->fit(800, 500);
-        Storage::delete('storage/sliders/' . $name);
+        $name = $this->attributes['image'] = basename($image->store('public/sliders/originals/'));
+        $img = Image::make('storage/sliders/originals/' . $name);
+        $img->fit(800, 300);
         $img->save('storage/sliders/' . $name);
+        $thumb = Image::make('storage/sliders/originals/' . $name);
+        $thumb->fit(300, 300);
+        $thumb->save('storage/sliders/thumbnails/' . $name);
     }
 
     /**
      * Get full path to image url.
      *
-     * @param  string $image
      * @return string
      */
-    public function getImageAttribute($image)
+    public function image()
     {
-        return asset('storage/sliders/' . $image);
+        return asset('storage/sliders/' . $this->image);
+    }
+
+    /**
+     * Get full path to image thumbnail url.
+     *
+     * @return string
+     */
+    public function thumbnail()
+    {
+        return asset('storage/sliders/thumbnails/' . $this->image);
+    }
+
+    /**
+     * Get full path to image thumbnail url.
+     *
+     * @return string
+     */
+    public function original()
+    {
+        return asset('storage/sliders/original/' . $this->image);
     }
 }
