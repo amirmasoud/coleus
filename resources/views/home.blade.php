a2264f76e1a1d6d8766cb83bcbee5a1816706363
amirmasoud <amirmasood33@gmail.com>
2017-07-24 17:38:03 +0430

    Fix covers url

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 <div class="col-xs-6 col-sm-4 col-md-2 pull-right">
     <a href="{{ route('authors.books', ['id'=>$author->id]) }}">
         <div class="thumbnail">
-            <img src="{{ asset('covers/' . $author->extra['cover']) }}" class="img-responsive" alt="hafez">
+            <img src="{{ asset('storage/covers/' . $author->extra['cover']) }}" class="img-responsive" alt="hafez">
             <div class="caption">
                 <h5 class="text-center">{{ $author->name }}</h5>
             </div>
