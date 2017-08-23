8f89f2484a20bc01332bf9758c1ac33440f229a2
amirmasoud <amirmasood33@gmail.com>
2017-08-23 10:30:03 +0430

    Fix class name typo

--- app/Http/Requests/Admin/ContentRequest.php
+++ app/Http/Requests/Admin/ContentRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\Admin;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class TableRequest extends FormRequest
+class ContentRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
