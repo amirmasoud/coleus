785edfd8235af6f03f4246ff5b0399b139d399e9
amirmasoud <amirmasood33@gmail.com>
2017-08-30 22:38:12 +0430

    Fix 'sh' name prefix

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,7 @@ Auth::routes();
 Route::get('/', 'HomeController@index')->name('home');
 require base_path('routes/admin/web.php');
 Route::get('{author}', 'AuthorController@books')->name('authors.books');
-Route::get('{author}/{book}/sh{index}/{parent?}', 'ReadController@show')->name('reads.show');
+Route::get('{author}/{book}/sh{index}/{parent?}', 'ReadController@show')
+    ->name('reads.show')
+    ->where('index', '[0-9]+');
 Route::get('{author}/{book}/{parent?}', 'BookController@list')->name('books.list');
