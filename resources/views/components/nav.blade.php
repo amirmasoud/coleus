344d769fddd3e219a5a79aa6cafae99ddc422fea
amirmasoud <amirmasood33@gmail.com>
2017-09-02 12:08:31 +0430

    Add beta to title

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,3 +1,3 @@
 <a class="custom-navbar-brand" href="{{ url('/') }}">
-    {{ config('app.name', 'Negar') }}
+    {{ config('app.name', 'Negar') }}<span style="display: block;font-size: 11px;">ßeta</span>
 </a>
--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>{{ config('app.name', 'Negar') }}.{{ config('app.domain', 'in') }}</title>
+        <title>نگارین</title>
         <!-- Styles -->
         <link href="{{ mix('css/app.css') }}" rel="stylesheet">
         <!-- Scripts -->
