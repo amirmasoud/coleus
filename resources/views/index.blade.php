f9bdf035b6a476db7d02d4d226844777adc674dd
amirmasoud <amirmasood33@gmail.com>
2018-10-18 15:15:13 +0330

    Add IRANYekan fonts

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -26,6 +26,7 @@ $polyfills = [
 
   <title>{{ __('general.negarin') }}</title>
 
+  <link href="https://fonts.googleapis.com/css?family=Markazi+Text" rel="stylesheet">
   <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
