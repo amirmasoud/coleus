01e72a9b181b2857d4f8cb8e2805a2deb12577e5
amirmasoud <amirmasood33@gmail.com>
2018-10-30 17:06:39 +0330

    Change to Apollo Client

--- resources/js/pages/welcome.vue
+++ resources/js/pages/welcome.vue
@@ -1,11 +1,11 @@
 <template>
-  <b-row v-if="writers">
+  <b-row v-if="users">
     <b-col cols="6"
            sm="4"
            md="4"
            lg="3"
            xl="2"
-           v-for="writer in writers"
+           v-for="writer in users"
            :key="writer.id">
       <b-card :title="writer.name"
               :img-src="writer.small || writer.photo_url"
@@ -21,6 +21,7 @@
 
 <script>
 import axios from 'axios'
+import gql from 'graphql-tag'
 
 export default {
   metaInfo () {
@@ -29,26 +30,22 @@ export default {
 
   data: () => ({
     title: window.config.appName,
-    writers: null
+    users: null
   }),
 
-  created () {
-    this.fetchFeaturedAuthors()
-  },
-
-  methods: {
-    async fetchFeaturedAuthors () {
-      const {data} = await axios.get((`graphql/?query=
-        query+FetchUsers {
-          users(sticky: true) {
-            id,
-            name,
-            small,
-            photo_url
-          }
-        }`).replace(/\s+/g, ''))
-      this.writers = data.data.users
-    }
+  apollo: {
+    // Query with parameters
+    users: {
+      // gql query
+      query: gql`query FetchUsers {
+        users(sticky: true) {
+          id,
+          name,
+          small,
+          photo_url
+        }
+      }`
+    },
   }
 }
 </script>
