2b09e6ab8ba5f21bb95ec0a3f841f23ec4b0825e
amirmasoud <amirmasood33@gmail.com>
2018-11-04 20:19:30 +0330

    Add sorts table

---
+++ database/migrations/2018_11_04_144738_create_sorts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSortsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sorts', function (Blueprint $table) {
+            $table->nestedSet();
+            $table->morphs('sortable');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sorts');
+    }
+}
