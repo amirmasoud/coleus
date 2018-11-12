3453afb3ebf55a34c0c85c37a9201adf47ea1c5f
amirmasoud <amirmasood33@gmail.com>
2018-11-12 22:30:20 +0330

    Add dataset storage path

--- config/filesystems.php
+++ config/filesystems.php
@@ -48,6 +48,11 @@ return [
             'root' => storage_path('app'),
         ],
 
+        'dataset' => [
+            'driver' => 'local',
+            'root' => storage_path('dataset'),
+        ],
+
         'admin' => [
             'driver' => 'local',
             'root' => public_path('uploads'),
