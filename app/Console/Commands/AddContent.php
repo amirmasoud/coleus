dca6cf2bc3499cbb2532429ba34abb94cde3d633
amirmasoud <amirmasood33@gmail.com>
2018-11-15 23:14:55 +0330

    Add ability to add sub-page to content

--- app/Console/Commands/AddContent.php
+++ app/Console/Commands/AddContent.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Models\Book;
+use App\Models\Page;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
@@ -16,7 +17,9 @@ class AddContent extends Command
      */
     protected $signature = 'add:content
                             {path : The path to dataset}
-                            {book : The book ID to content add to}';
+                            {book : The book ID to content add to}
+                            {parent? : The parent of the content}
+                            {--R|refresh : Refresh book\'s content before import}';
 
     /**
      * The console command description.
@@ -42,29 +45,66 @@ class AddContent extends Command
      */
     public function handle()
     {
+        ini_set('memory_limit', '-1');
+
+        if ($this->option('refresh')) {
+            Book::find($this->argument('book'))->pages()->delete();
+        }
+
         if (!Book::find($this->argument('book'))->pages()->count() || $this->confirm('Do you wish to continue?')) {
             $guy = Storage::disk('dataset')->allFiles($this->argument('path'));
-            sort($guy);
+
+            $bar = $this->output->createProgressBar(count($guy) + 1);
+
+            sort($guy, SORT_NATURAL);
+
+            $bar->start();
+
+            if ($this->argument('parent')) {
+                $parent = Book::find($this->argument('book'))
+                    ->pages()
+                    ->create([
+                        'title' => $this->argument('parent'),
+                        'content' => ':empty'
+                    ]);
+            }
+
+            $bar->advance();
+
             foreach ($guy as $poem) {
                 if (Str::endsWith($poem, '.json')) {
                     $file = json_decode(Storage::disk('dataset')->get($poem));
                     $title = '';
                     $content = '';
                     foreach ($file->text as $key => $part) {
-                        if (! $key) { // First item - Generate title of page
+                        if (! $key && property_exists($part, 'm1')) { // First item - Generate title of page
                             $title = $part->m1;
                         }
-                        $content .= '<p class="col-12 col-md-6" style="text-align: right;">' . $part->m1 . '</p>';
-                        $content .= '<p class="col-12 col-md-6" style="text-align: right;">' . $part->m2 . '</p>';
+
+                        if (property_exists($part, 'm1') && property_exists($part, 'm2')) {
+                            $content .= '<p class="col-12 col-md-6" style="text-align: right;">' . $part->m1 . '</p>';
+                            $content .= '<p class="col-12 col-md-6" style="text-align: right;">' . $part->m2 . '</p>';
+                        }
+
+                        if (property_exists($part, 't1') && property_exists($part, 't2')) {
+                            $content .= '<p class="t col-12 col-md-6" style="text-align: right;">' . $part->t1 . '</p>';
+                            $content .= '<p class="t col-12 col-md-6" style="text-align: right;">' . $part->t2 . '</p>';
+                        }
                     }
-                    \App\Models\Book::find($this->argument('book'))
+
+                    Book::find($this->argument('book'))
                         ->pages()
                         ->create([
-                            'title' => $title,
-                            'content' => $content
+                            'title'   => $title,
+                            'content' => $content,
+                            'parent' => $this->argument('parent') ? $parent : null
                         ]);
                 }
+                $bar->advance();
             }
+
+            $bar->finish();
+
             $this->info('Data added to book.');
         } else {
             $this->error('Operation cancelled!');
