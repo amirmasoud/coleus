7fb88518a43581d58a314e4267e8e27ef2b2490b
amirmasoud <amirmasood33@gmail.com>
2018-11-24 01:01:15 +0330

    Add custom title, count and persian flag

--- app/Console/Commands/AddContent.php
+++ app/Console/Commands/AddContent.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use App\Models\Book;
 use App\Models\Page;
+use App\Models\Paragraph;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
@@ -19,7 +20,10 @@ class AddContent extends Command
                             {path : The path to dataset}
                             {book : The book ID to content add to}
                             {parent? : The parent of the content}
-                            {--R|refresh : Refresh book\'s content before import}';
+                            {title? : The title of the content}
+                            {--R|refresh : Refresh book\'s content before import}
+                            {--C|count : Count title on indexing}
+                            {--P|persian : Persian numbers}';
 
     /**
      * The console command description.
@@ -48,6 +52,12 @@ class AddContent extends Command
         ini_set('memory_limit', '-1');
 
         if ($this->option('refresh')) {
+            $this->info('Refreshing book' . PHP_EOL);
+            $ids = array_map(function($item) {
+                return $item['id'];
+            }, Book::find($this->argument('book'))->pages()->get(['id'])->toArray());
+
+            Paragraph::whereIn('page_id', $ids)->delete();
             Book::find($this->argument('book'))->pages()->delete();
         }
 
@@ -70,14 +80,29 @@ class AddContent extends Command
 
         $bar->advance();
 
+        $i = 1;
         foreach ($guy as $poem) {
             if (Str::endsWith($poem, '.json')) {
                 $file = json_decode(Storage::disk('dataset')->get($poem));
                 $title = '';
                 $content = '';
                 foreach ($file->text as $key => $part) {
-                    if (! $key && property_exists($part, 'm1')) { // First item - Generate title of page
-                        $title = $part->m1;
+                    if (! $key) { // First item
+                        if (!$this->argument('title') && property_exists($part, 'm1')) { // First m2
+                            $title = $part->m1;
+                        } else { // Custom title
+                            if ($this->option('persian')) {
+                                $str = $i;
+                                $western_persian = array('0','1','2','3','4','5','6','7','8','9');
+                                $eastern_persian = array('٠','١','٢','٣','٤','٥','٦','٧','٨','٩');
+
+                                $str = str_replace($western_persian, $eastern_persian, $str);
+                                $i++;
+                            }
+                            $title = $this->option('count')
+                                        ? $this->argument('title') . ' ' . ($this->option('persian') ? $str : $i++)
+                                        : $this->argument('title');
+                        }
                     }
 
                     if (property_exists($part, 'm1') && property_exists($part, 'm2')) {
