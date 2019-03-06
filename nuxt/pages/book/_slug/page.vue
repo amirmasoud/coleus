a46cbaf6d8261410f62edafc864c794277d18501
amirmasoud <amirmasood33@gmail.com>
2019-03-06 18:13:07 +0330

    Fix DynamicScroller

--- nuxt/pages/book/_slug/page.vue
+++ nuxt/pages/book/_slug/page.vue
@@ -1,35 +1,34 @@
 <template>
   <div class="flex flex-wrap" v-if="book">
     <div :class="{'hidden': !showMenu }" class="w-full lg:w-1/4 mt-2 pl-2 sticky self-start lg:block" style="top: 72px;">
-      <div class="book-contents rounded overflow-hidden p-3 bg-white overflow-y-auto overflow-x-hidden h-screen-91" v-if="book">
-        <no-ssr>
-          <DynamicScroller
-            :items="book.pages"
-            :prerender="100"
-            :min-item-size="54"
-            ref="scroller"
-          >
-            <template slot-scope="{ item, index, active }">
-              <DynamicScrollerItem
-                :item="item"
-                :active="active"
-                :size-dependencies="[ item.title ]"
-                :data-index="index"
+      <div class="book-contents rounded p-3 bg-white overflow-y-auto overflow-x-hidden h-screen-91" v-if="book">
+        <DynamicScroller
+          :items="book.pages"
+          :prerender="100"
+          :min-item-size="54"
+          ref="scroller"
+          class="h-full"
+        >
+          <template slot-scope="{ item, index, active }">
+            <DynamicScrollerItem
+              :item="item"
+              :active="active"
+              :size-dependencies="[ item.title ]"
+              :data-index="index"
+            >
+              <strong v-if="item.is_header" @click.prevent="closeMenu" class="block py-2 px-4 mb-1">{{ item.title }}</strong>
+              <NuxtLink
+                v-else
+                class="no-underline text-black block py-2 px-4 border border-transparent rounded-sm mb-1 hover:bg-grey-lightest transition-delay-none transition-timing-linear transition-property-bg transition-fast"
+                :class="{ 'bg-grey-lightest font-bold border border-grey-light': item.id == $route.params.id }"
+                :to="{ name: 'book-slug-page-id', params: { slug: $route.params.slug, id: item.id }}"
+                @click.native="closeMenu"
               >
-                <strong v-if="item.is_header" @click.prevent="closeMenu" class="block py-2 px-4 mb-1">{{ item.title }}</strong>
-                <NuxtLink
-                  v-else
-                  class="no-underline text-black block py-2 px-4 border border-transparent rounded-sm mb-1 hover:bg-grey-lightest transition-delay-none transition-timing-linear transition-property-bg transition-fast"
-                  :class="{ 'bg-grey-lightest font-bold border border-grey-light': item.id == $route.params.id }"
-                  :to="{ name: 'book-slug-page-id', params: { slug: $route.params.slug, id: item.id }}"
-                  @click.native="closeMenu"
-                >
-                  {{ item.title }}
-                </NuxtLink>
-              </DynamicScrollerItem>
-            </template>
-          </DynamicScroller>
-        </no-ssr>
+                {{ item.title }}
+              </NuxtLink>
+            </DynamicScrollerItem>
+          </template>
+        </DynamicScroller>
       </div>
     </div>
 
