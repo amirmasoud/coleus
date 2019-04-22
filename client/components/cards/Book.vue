fdea64d8b74f0908541bc2c6231c529891021f43
amirmasoud <amirmasood33@gmail.com>
2019-04-23 00:46:51 +0430

    Apply new colors

--- client/components/cards/Book.vue
+++ client/components/cards/Book.vue
@@ -1,6 +1,11 @@
 <template>
-  <NuxtLink :to="{ name: 'book-slug-page-id', params: { slug: book.slug, id: book.start }}" class="no-underline text-black">
-    <div class="max-w-sm rounded overflow-hidden shadow-md hover:shadow-lg transition bg-white">
+  <NuxtLink
+    :to="{ name: 'book-slug-page-id', params: { slug: book.slug, id: book.start }}"
+    class="no-underline text-deep-space-sparkle hover:outer-space"
+  >
+    <div
+      class="max-w-sm rounded overflow-hidden border border-x11-grey hover:border-silver-chalice transition bg-alabster hover:bg-almond"
+    >
       <img :src="book.cover" class="hidden">
       <no-ssr>
         <progressive-img
@@ -10,9 +15,7 @@
         />
       </no-ssr>
       <div class="px-4 py-2">
-        <div class="text-lg mb-1 py-1">
-          {{ book.title }}
-        </div>
+        <div class="text-lg mb-1 py-1">{{ book.title }}</div>
       </div>
     </div>
   </NuxtLink>
