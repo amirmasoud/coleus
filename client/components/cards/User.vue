b1b9169b155c181f5da4a0c4645f157790bbe584
amirmasoud <amirmasood33@gmail.com>
2019-04-24 12:04:37 +0430

    Add itemprops and elements for User

--- client/components/cards/User.vue
+++ client/components/cards/User.vue
@@ -1,24 +1,28 @@
 <template>
-  <NuxtLink
-    :to="{ name: 'profile', params: { username: user.username }}"
-    class="no-underline text-deep-space-sparkle hover:outer-space"
-  >
-    <div
-      class="max-w-sm rounded overflow-hidden border border-x11-grey hover:border-silver-chalice transition bg-alabster hover:bg-almond"
+  <span itemscope itemtype="http://schema.org/Person">
+    <NuxtLink
+      itemprop="url"
+      :to="{ name: 'profile', params: { username: user.username }}"
+      class="no-underline text-deep-space-sparkle hover:outer-space"
     >
-      <img :src="user.cover" class="hidden">
-      <no-ssr>
-        <progressive-img
-          :src="user.small || user.photo_url"
-          :placeholder="user.placeholder"
-          :aspect-ratio="1"
-        />
-      </no-ssr>
-      <div class="px-4 py-2">
-        <div class="text-normal mb-1 py-1">{{ user.name }}</div>
+      <div
+        class="max-w-sm rounded overflow-hidden border border-x11-grey hover:border-silver-chalice transition bg-alabster hover:bg-almond"
+      >
+        <img itemprop="image" :src="user.small || user.photo_url" class="hidden">
+        <p itemprop="description" class="hidden">{{ user.bio }}</p>
+        <no-ssr>
+          <progressive-img
+            :src="user.small || user.photo_url"
+            :placeholder="user.placeholder"
+            :aspect-ratio="1"
+          />
+        </no-ssr>
+        <div class="px-4 py-2">
+          <div itemprop="name" class="text-normal mb-1 py-1">{{ user.name }}</div>
+        </div>
       </div>
-    </div>
-  </NuxtLink>
+    </NuxtLink>
+  </span>
 </template>
 
 <script>
