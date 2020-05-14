ef42b28e65b10dd4a877e1bca8f5a09d938e31a6
Amirmasoud <amirmasood33@gmail.com>
2020-05-14 14:14:59 +0430

    Scaffold search API

---
+++ backend/app/Http/Controllers/SearchController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use App\Book;
+use App\Page;
+use App\Block;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Database\QueryException;
+
+class SearchController extends Controller
+{
+    public function query(Request $request)
+    {
+        dd(User::search('sghl')->get());
+        if ($request->filled('q')) {
+            try {
+                return [
+                    'users' => User::search($request->q)->get(),
+                    'books' => Book::search($request->q)->get(),
+                    'pages' => Page::search($request->q)->get(),
+                    'blocks' => Block::search($request->q)->get()
+                ];
+            } catch (QueryException $e) {
+                return [];
+            }
+        } else {
+            return [];
+        }
+    }
+}
