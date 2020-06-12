056df27683f3e272762dde88a0086cfc742ae5f3
Amirmasoud <amirmasood33@gmail.com>
2020-06-12 21:31:11 +0430

    Error in fa

--- frontend/layouts/error.vue
+++ frontend/layouts/error.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="container mx-auto">
     <h1>{{ error.statusCode }}</h1>
-    <h1 v-if="error.statusCode === 404">Page not found</h1>
-    <h1 v-else>An error occurred</h1>
+    <h1 v-if="error.statusCode === 404">این صفحه پیدا نشد</h1>
+    <h1 v-else>خطایی رخ داد</h1>
     <h3>{{ error.message }}</h3>
-    <nuxt-link to="/">Home page</nuxt-link>
+    <nuxt-link to="/">خانه</nuxt-link>
   </div>
 </template>
 
