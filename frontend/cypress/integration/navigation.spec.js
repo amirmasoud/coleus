3fd28e8791bbd2a3bda5c01e2e100a9c54576836
Amirmasoud <amirmasood33@gmail.com>
2020-06-26 22:30:57 +0430

    Cypress

---
+++ frontend/cypress/integration/navigation.spec.js
@@ -0,0 +1,24 @@
+context('Navigation', () => {
+  it('lands on home', () => {
+    cy.visit('/')
+    cy.url().should('match', /^/)
+  })
+
+  it('lands on books', () => {
+    cy.visit('books')
+    cy.url().should('match', /books/)
+  })
+
+  it('lands on profile', () => {
+    cy.visit("Parvin-E'tesami")
+    cy.url().should('match', /Parvin-E'tesami|Parvin-E%27tesami/)
+  })
+
+  it('lands on page', () => {
+    cy.visit("Parvin-E'tesami/Divan---E-Parvin-E'Tesami/1/2/?page=1")
+    cy.url().should(
+      'match',
+      /Parvin-E'tesami\/Divan---E-Parvin-E'Tesami\/1\/2\/\?page=1|Parvin-E%27tesami\/Divan---E-Parvin-E%27Tesami\/1\/2\/\?page=1/
+    )
+  })
+})
