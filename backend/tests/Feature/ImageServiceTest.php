3cdb32fd5439ccb050b955663daeeeee9772aba8
Amirmasoud <amirmasood33@gmail.com>
2020-07-02 23:51:08 +0430

    Add feature test for ImageService

---
+++ backend/tests/Feature/ImageServiceTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\UploadedFile;
+use App\Services\ImageService;
+use Tests\TestCase;
+
+class ImageServiceTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testImagePublicUrl()
+    {
+        $image = new ImageService();
+
+        // ImgProxy uses MinIO not Laravel storage/framework/testing/ directory.
+        $path = Storage::put('test', UploadedFile::fake()->image('test.jpeg', 1024, 1024));
+        Storage::assertExists($path);
+
+        $url = $image->publicUrl('public/' . $path, 512, 512);
+        $this->assertFalse(filter_var($url, FILTER_VALIDATE_URL) === false);
+
+        // failed to open stream: Connection refused
+        // $this->get($url)
+        //     ->assertOk()
+        //     ->assertHeader('content-type', 'image/jpeg');
+    }
+}
