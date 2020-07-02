5982df8bb148c11e5cc2076c496a06ae181025f0
Amirmasoud <amirmasood33@gmail.com>
2020-07-03 00:47:53 +0430

    WIP

--- admin/src/App.js
+++ admin/src/App.js
@@ -5,7 +5,7 @@ import MenuBookIcon from "@material-ui/icons/MenuBook";
 import ViewQuiltIcon from "@material-ui/icons/ViewQuilt";
 import { Admin, Resource, ListGuesser, EditGuesser } from "react-admin";
 import hasuraDataProvider from "ra-data-hasura";
-import { UserList } from "./components/users";
+import { UserList, UserEdit } from "./components/users";
 
 // The following components are created when following the react-admin tutorial
 // import { PostList, PostEdit, PostCreate, PostShow } from "./posts";
@@ -32,7 +32,7 @@ const App = () => (
       show={PostShow}
     />
     <Resource name="users" icon={UserIcon} list={UserList} /> */}
-    <Resource name="users" list={UserList} edit={EditGuesser} icon={UserIcon} />
+    <Resource name="users" list={UserList} edit={UserEdit} icon={UserIcon} />
     <Resource name="books" list={ListGuesser} icon={BookIcon} />
     <Resource name="pages" list={ListGuesser} icon={MenuBookIcon} />
     <Resource name="blocks" list={ListGuesser} icon={ViewQuiltIcon} />
