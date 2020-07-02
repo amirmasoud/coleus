fe31f8eaf49dc59b63e794831c95888e3233ac5e
Amirmasoud <amirmasood33@gmail.com>
2020-07-03 00:34:30 +0430

    icons

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
-import PostIcon from "@material-ui/icons/Book";
+import BookIcon from "@material-ui/icons/Book";
 import UserIcon from "@material-ui/icons/Group";
+import MenuBookIcon from "@material-ui/icons/MenuBook";
+import ViewQuiltIcon from "@material-ui/icons/ViewQuilt";
 import { Admin, Resource, ListGuesser, EditGuesser } from "react-admin";
 import hasuraDataProvider from "ra-data-hasura";
 import { UserList } from "./components/users";
@@ -30,10 +32,10 @@ const App = () => (
       show={PostShow}
     />
     <Resource name="users" icon={UserIcon} list={UserList} /> */}
-    <Resource name="users" list={UserList} edit={EditGuesser} />
-    <Resource name="books" list={ListGuesser} />
-    <Resource name="pages" list={ListGuesser} />
-    <Resource name="blocks" list={ListGuesser} />
+    <Resource name="users" list={UserList} edit={EditGuesser} icon={UserIcon} />
+    <Resource name="books" list={ListGuesser} icon={BookIcon} />
+    <Resource name="pages" list={ListGuesser} icon={MenuBookIcon} />
+    <Resource name="blocks" list={ListGuesser} icon={ViewQuiltIcon} />
   </Admin>
 );
 
