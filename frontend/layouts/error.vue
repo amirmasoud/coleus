cf46017ad91690c8e4c1c47ad6bdcd38a2feb155
Amirmasoud <amirmasood33@gmail.com>
2020-07-10 22:47:44 +0430

    Err. pages

--- frontend/layouts/error.vue
+++ frontend/layouts/error.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="container mx-auto p-4">
     <coleus-svg-404 v-if="error.statusCode === 404" class="max-w-3xl mx-auto" />
-
-    <h1 v-if="error.statusCode !== 404">{{ error.statusCode }}</h1>
+    <coleus-svg-500 v-else class="max-w-3xl mx-auto" />
 
     <h3 class="mt-20 text-center text-2xl font-thin">{{ error.message }}</h3>
+    <p class="text-center font-hairline italic">Err. {{ error.statusCode }}</p>
 
     <nuxt-link to="/" class="block text-center font-bold mt-4"
       >رفتن به خانه ←</nuxt-link
